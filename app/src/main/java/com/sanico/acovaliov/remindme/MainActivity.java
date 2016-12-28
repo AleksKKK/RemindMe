a2b0e53dbaf482f50a3f98b863f3321d70c1e19d
AleksKKK <ads77Fxc3de>
2016-12-28 09:43:11 +0200

    Created main activity

---
+++ app/src/main/java/com/sanico/acovaliov/remindme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sanico.acovaliov.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by acovaliov on 28-Dec-16.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
