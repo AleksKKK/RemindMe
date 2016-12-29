774dcf2660a99d471c5f3b876d05b4241c6d5d89
AleksKKK <ads77Fxc3de>
2016-12-29 09:34:23 +0200

    Added toolbar

--- app/src/main/java/com/sanico/acovaliov/remindme/MainActivity.java
+++ app/src/main/java/com/sanico/acovaliov/remindme/MainActivity.java
@@ -2,6 +2,7 @@ package com.sanico.acovaliov.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by acovaliov on 28-Dec-16.
@@ -9,11 +10,22 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        initToolbar();
+    }
+
+    private void initToolbar()
+    {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
