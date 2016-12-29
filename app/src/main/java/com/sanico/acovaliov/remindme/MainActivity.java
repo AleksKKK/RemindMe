cc6a79b6fba62a314bbe8813628a361953c3c339
AleksKKK <ads77Fxc3de>
2016-12-29 18:12:14 +0200

    Added navigation draw

--- app/src/main/java/com/sanico/acovaliov/remindme/MainActivity.java
+++ app/src/main/java/com/sanico/acovaliov/remindme/MainActivity.java
@@ -2,18 +2,20 @@ package com.sanico.acovaliov.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 /**
  * Created by acovaliov on 28-Dec-16.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
@@ -23,7 +25,7 @@ public class MainActivity extends Activity {
 
     private void initToolbar()
     {
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar = (Toolbar)findViewById(R.id.main_toolbar);
         toolbar.setTitle(R.string.app_name);
 
         toolbar.inflateMenu(R.menu.menu);
